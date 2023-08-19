9c7ddb97fa3cb1b40151b6ed2634914603442c09
JIGAR SOLANKI <79460184+jigarsolanki0212@users.noreply.github.com>
2023-08-19 15:31:47 +0530

    Changis API Data
    - Changes food data of API to mock data

--- FoodRedux/reducer/userReducer.js
+++ FoodRedux/reducer/userReducer.js
@@ -1,8 +1,110 @@
 import types from '../actions/types';
 
 const initialState = {
-  data: [],
-  categories:[],
+  data: [
+    {
+      id: '1',
+      title: 'Orange',
+      image_url:
+        'https://freepngimg.com/thumb/orange/5-orange-png-image-download-thumb.png',
+      rating: 1,
+      price: 2.49,
+      Categories: 'Fruits',
+      description: 'The Cliffs of Cinque Terre',
+    },
+
+    {
+      id: '2',
+      title: 'Mango',
+      image_url:
+        'https://freepngimg.com/thumb/mango/9-2-mango-transparent-thumb.png',
+      rating: 3,
+      price: 3.99,
+      Categories: 'Fruits',
+      description: 'Rialto Bridge, Venezia, Italy',
+    },
+    {
+      id: '3',
+      title: 'Black Raspberries',
+      image_url:
+        'https://freepngimg.com/thumb/raspberries/27782-3-black-raspberries-photos-thumb.png',
+      rating: 2.5,
+      price: 1.99,
+      Categories: 'Vegetables',
+      description: 'Rialto Bridge, Venezia, Italy',
+    },
+    {
+      id: '4',
+      title: 'Pomegranate',
+      image_url:
+        'https://freepngimg.com/thumb/pomegranate/22853-4-pomegranate-photos-thumb.png',
+      rating: '5',
+      price: 2.99,
+      Categories: 'Vegetables',
+      description: 'Rialto Bridge, Venezia, Italy',
+    },
+    {
+      id: '5',
+      title: 'Cherry ',
+      image_url:
+        'https://freepngimg.com/thumb/cherry/35763-7-cherry-fruit-file-thumb.png',
+      rating: '1',
+      price: 1.49,
+      Categories: 'Fast food',
+      description: 'Rialto Bridge, Venezia, Italy',
+    },
+    {
+      id: '6',
+      title: 'Green Grapes',
+      image_url:
+        'https://freepngimg.com/thumb/grapes/36616-3-green-grapes-thumb.png',
+      rating: '2',
+      price: 9,
+      Categories: 'Fast food',
+      description: 'Rialto Bridge, Venezia, Italy',
+    },
+    {
+      id: '7',
+      title: 'Papaya',
+      image_url:
+        'https://freepngimg.com/thumb/papaya/24575-9-papaya-file-thumb.png',
+      rating: '2',
+      price: 12.2,
+      Categories: 'Fast food',
+      description: 'Rialto Bridge, Venezia, Italy',
+    },
+    {
+      id: '8',
+      title: 'Watermelon',
+      image_url:
+        'https://freepngimg.com/thumb/watermelon/1-watermelon-png-image-thumb.png',
+      rating: '2',
+      price: 50,
+      Categories: 'Fast food',
+      description: 'Rialto Bridge, Venezia, Italy',
+    },
+    {
+      id: '9',
+      title: 'Raspberry',
+      image_url:
+        'https://freepngimg.com/thumb/raspberry/37066-8-raspberry-photos-thumb.png',
+      rating: '2',
+      price: 13,
+      Categories: 'Fast food',
+      description: 'Rialto Bridge, Venezia, Italy',
+    },
+    {
+      id: '10',
+      title: 'Pineapple',
+      image_url:
+        'https://freepngimg.com/thumb/pineapple/5-pineapple-png-image-download-thumb.png',
+      rating: '2',
+      price: 45,
+      Categories: 'Fast food',
+      description: 'Rialto Bridge, Venezia, Italy',
+    },
+  ],
+  categories: [],
 };
 
 export default (state = initialState, action) => {
@@ -11,7 +113,7 @@ export default (state = initialState, action) => {
       return {
         ...state,
         data: action.payload.data,
-        categories: action.payload.categories
+        categories: action.payload.categories,
       };
     }
     case types.API_CALL_START: {
@@ -19,7 +121,7 @@ export default (state = initialState, action) => {
         ...state,
       };
     }
-   
+
     default: {
       return state;
     }
--- src/views/screen/HomeScreen/HomeScreen.js
+++ src/views/screen/HomeScreen/HomeScreen.js
@@ -62,6 +62,7 @@ const HomeScreen = ({navigation}) => {
 
       dispatch(apiFinish({fruits, uniqueCategories}));
     }
+    g;
   };
 
   const Item = ({item, onPress, backgroundColor, textColor}) => (
